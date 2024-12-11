e694fd5efd465a40505a94e7a60a7e85909fe39e
Mohammad Moradi <MrMohammad.Moradi@yahoo.com>
2024-12-11 23:01:24 +0330

    20241211,Moradi: Add base of AboutMe page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,6 +1,32 @@
-
 export const AboutMe = () => {
-  return (
-    <div>AboutMe</div>
-  )
-}
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - 2001;
+    console.log(age);
+    return (
+        <div className="h-full overflow-y-auto">
+            <div className="flex flex-col p-5 cursor-default">
+                <div className="flex flex-col py-5 px-3">
+                    <h3 className="font-bold text-2xl text-blue-950">
+                        First, a full introduction of myself :
+                    </h3>
+                    <p className=""></p>
+                    <p className=""></p>
+                </div>
+                <div className="flex flex-col py-5 px-3">
+                    <h3 className="font-bold text-2xl text-blue-950">
+                        Second, my entrance into the programming world :
+                    </h3>
+                    <p className=""></p>
+                    <p className=""></p>
+                </div>
+                <div className="flex flex-col py-5 px-3">
+                    <h3 className="font-bold text-2xl text-blue-950">
+                        Third, what I am looking for now :
+                    </h3>
+                    <p className=""></p>
+                    <p className=""></p>
+                </div>
+            </div>
+        </div>
+    );
+};
